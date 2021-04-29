97cb8aca68e7efcc33c2d7f2049399d3e6ec5734
Paolo Ambrosio <git@paolo.ambrosio.name>
2021-04-29 08:53:44 +0100

    HUD projects setup

---
+++ src/HUD/Pipeline/WebSocketTelemetryNode.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks.Dataflow;
+
+namespace RaceDirector.Plugin.HUD.Pipeline
+{
+    using ILiveTelemetry = RaceDirector.Pipeline.Telemetry.V0.ILiveTelemetry;
+    using IRunningGame = RaceDirector.Pipeline.GameMonitor.V0.IRunningGame;
+
+    public class WebSocketTelemetryNode
+    {
+        public ITargetBlock<IRunningGame> RunningGameTarget
+        {
+            get;
+        }
+
+        public ITargetBlock<ILiveTelemetry> LiveTelemetryTarget
+        {
+            get;
+        }
+    }
+}
---
+++ tests/HUD.Tests/WebSocketTelemetryNodeTest.cs
@@ -0,0 +1,15 @@
+using Xunit;
+using Xunit.Categories;
+
+namespace HUD.Tests.Pipeline
+{
+    [IntegrationTest]
+    public class WebSocketTelemetryNodeTest
+    {
+        [Fact]
+        public void Guilty()
+        {
+
+        }
+    }
+}
