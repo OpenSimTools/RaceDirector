3251b2bec7e03af22db9d6bf619bfb69a662df85
Paolo Ambrosio <git@paolo.ambrosio.name>
2021-05-21 07:39:45 +0100

    [WIP] Meh..,

--- src/RaceDirector.Interface/Pipeline/Telemetry/LiveTelemetry.cs
+++ src/RaceDirector.Interface/Pipeline/Telemetry/LiveTelemetry.cs
@@ -2,5 +2,6 @@
 
 namespace RaceDirector.Pipeline.Telemetry
 {
+    // TODO this is in the interface to make testing easier, but it might be abused
     public record LiveTelemetry(TimeSpan SimulationTime) : V0.ILiveTelemetry;
 }
