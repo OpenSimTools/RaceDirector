39af6f1364cac1c03fc1f70581e2c53404e025fd
Paolo Ambrosio <git@paolo.ambrosio.name>
2022-07-22 20:52:16 +0100

    R3E dashboard API version configurable (#56)

--- src/Plugins/HUD/Plugin.cs
+++ src/Plugins/HUD/Plugin.cs
@@ -6,17 +6,17 @@ namespace RaceDirector.Plugin.HUD
 {
     public class Plugin : PluginBase<Plugin.Configuration>
     {
+        public class Configuration
+        {
+            public DashboardServer.Config DashboardServer { get; set; } = null!;
+        }
+
         protected override void Init(Configuration configuration, IServiceCollection services)
         {
             services
                 .AddSingletonWithInterfaces(_ => configuration.DashboardServer)
                 .AddTransientWithInterfaces<DashboardServer>()
                 .AddTransientWithInterfaces<WebSocketTelemetryNode>();
-        }
-
-        public class Configuration
-        {
-            public DashboardServer.Config DashboardServer { get; set; } = null!;
         }
     }
 }
