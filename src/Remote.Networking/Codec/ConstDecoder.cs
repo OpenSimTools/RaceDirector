276ea76ef30c52b85ec0ae0c27a6cb346f063cf7
Paolo Ambrosio <git@paolo.ambrosio.name>
2022-08-15 06:53:48 +0100

    Failing test

---
+++ src/Remote.Networking/Codec/ConstDecoder.cs
@@ -0,0 +1,11 @@
+﻿namespace RaceDirector.Remote.Networking.Codec;
+
+public record ConstDecoder<T>(T Value) : IDecoder<T>
+{
+    public T Decode(ReadOnlyMemory<byte> payload) => Value;
+}
+
+public static class ConstDecoder
+{
+    public static IDecoder<Nothing> Nothing => new ConstDecoder<Nothing>(new Nothing());
+}
---
+++ tests/Remote.Networking.Tests/Client/SyncWsClientTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using RaceDirector.Remote.Networking;
+using RaceDirector.Remote.Networking.Client;
+using RaceDirector.Remote.Networking.Codec;
+using Xunit;
+using Xunit.Categories;
+
+namespace Remote.Networking.Tests.Client;
+
+[IntegrationTest]
+public class SyncWsClientTest
+{
+    [Fact]
+    public void RequiresProtocol()
+    {
+        var address = IPAddress.Loopback.ToString();
+
+        Assert.Throws<UriFormatException>(() =>
+        {
+            new SyncWsClient<Nothing, Nothing>(address, ConstDecoder.Nothing.ToCodec(), TimeSpan.Zero);
+        });
+    }
+
+    [Fact]
+    public void AcceptsIpAddress()
+    {
+        var address = IPAddress.Loopback.ToString();
+
+        var client = new SyncWsClient<Nothing, Nothing>($"ws://{address}", ConstDecoder.Nothing.ToCodec(), TimeSpan.Zero);
+
+        Assert.Equal(address,client.Address);
+        Assert.Equal(80, client.Port);
+    }
+
+    [Fact]
+    public void AcceptsHostname()
+    {
+        var address = "localhost";
+
+        var client =
+            new SyncWsClient<Nothing, Nothing>($"ws://{address}", ConstDecoder.Nothing.ToCodec(), TimeSpan.Zero);
+
+        Assert.Equal(IPAddress.Loopback.ToString(), client.Address);
+        Assert.Equal(80, client.Port);
+    }
+}
